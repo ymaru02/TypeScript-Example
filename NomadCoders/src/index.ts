f871a79c43f4e473abbe173b6aa95d8b40a6aa5d
Ymaru02 <dyun022@gmail.com>
2022-04-17 10:22:30 +0900

    #3 TypeScript class

---
+++ NomadCoders/src/index.ts
@@ -0,0 +1,32 @@
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
+}
+
+const lynn = new Human("Lynn", 18, "female");
+
+// interface Human {
+//   name: string;
+//   age: number;
+//   gender: string;
+//}
+
+const person = {
+  name: "nicolas",
+  gender: "Male",
+  age: 22,
+};
+
+const sayHi = (person: Human): string => {
+  return `Hello ${person.name} , you are ${person.age}, you are a ${person.gender}`;
+};
+
+console.log(sayHi(lynn));
+
+export {};
