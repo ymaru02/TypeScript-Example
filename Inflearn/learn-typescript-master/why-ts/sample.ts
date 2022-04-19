d948be37565b70cb77c1a03e6f6ff9482fee93ca
Ymaru02 <dyun022@gmail.com>
2022-04-19 23:39:07 +0900

    Inflearn init

---
+++ Inflearn/learn-typescript-master/why-ts/sample.ts
@@ -0,0 +1,6 @@
+function add(a: number, b: number): number {
+  return a + b;
+}
+
+let result = add(10, 20);
+result.toLocaleString();
---
+++ Inflearn/learn-typescript-master/why-ts/smaple.js
@@ -0,0 +1,6 @@
+function sum(a, b) {
+  return a + b;
+}
+
+let result1 = sum(10, "20");
+result1.toLocalString();
